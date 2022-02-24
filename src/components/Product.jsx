5b2b56c11cb2594f6dcacc75a4d220f8d0d19fa6
Alexander Celis <dacelis0@misena.edu.co>
2022-02-24 11:29:31 -0500

    add: gallery lightbox

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import allImages from '../data'
 import ProductCounter from './ProductCounter'
 import '../styles/components/product.css'
+import SimpleReactLightbox, { SRLWrapper } from 'simple-react-lightbox'
 
-const Thumbnail = ({ urlImg }) => {
+const Thumbnail = ({ urlImg, active }) => {
   return (
-    <figure>
+    <figure className={active}>
       <img className='img-fluid' src={urlImg} alt='sneaker thumbnail' />
     </figure>
   )
@@ -13,17 +14,26 @@ const Thumbnail = ({ urlImg }) => {
 
 const Product = () => {
   const [mainPhoto, setMainPhoto] = useState(allImages[0].img)
+  const deletePhoto = allImages.filter(({ img }) => img !== mainPhoto)
+
   return (
     <section className='product'>
       <div className='container row-grid'>
         <aside className='product-photos'>
-          <figure className='main-photo'>
-            <img className='img-fluid' src={mainPhoto} alt='sneaker' />
-          </figure>
+          <SimpleReactLightbox>
+            <SRLWrapper>
+              <figure className='main-photo'>
+                <img className='img-fluid' src={mainPhoto} alt='sneaker' />
+              </figure>
+              <div style={{ display: 'none' }}>
+                {deletePhoto.map(({ img }, index) => <img key={index} src={img} alt='foto' />)}
+              </div>
+            </SRLWrapper>
+          </SimpleReactLightbox>
           <ul className='thumbnails'>
             {allImages.map(({ thumb, img }, index) =>
               <li key={index} onClick={() => setMainPhoto(img)}>
-                <Thumbnail urlImg={thumb} />
+                <Thumbnail active={(mainPhoto === img) ? 'active' : ''} urlImg={thumb} />
               </li>
             )}
           </ul>
